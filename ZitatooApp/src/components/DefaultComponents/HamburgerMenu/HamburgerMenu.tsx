3b9474db6efe37fdcb1331c132630ae8e4b23271
Lexux3000 <alexandr.roman201@gmail.com>
2025-06-09 20:01:02 +0200

    feat: index.html

--- ZitatooApp/src/components/DefaultComponents/HamburgerMenu/HamburgerMenu.tsx
+++ ZitatooApp/src/components/DefaultComponents/HamburgerMenu/HamburgerMenu.tsx
@@ -8,7 +8,7 @@ const HamburgerIcon = () => (
     <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
   </svg>
 );
-
+// in future will onClick show blurred HamburgerMenu
 const HamburgerMenu = () => {
   return (
     <Menu.Root>
